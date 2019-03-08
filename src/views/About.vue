7346d6b3c520a80fcf4c5da6066202e6b2144d8e
店小二 <747026542@qq.com>
2019-03-08 19:58:35 +0800

    更新Demo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,51 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="parent">
+    <h2>Simple Node Demo 2.0</h2>
+    <div id="myDiagram" class="myCanvas"></div>
   </div>
 </template>
 
 <script>
+import { Diagram, Node, Panel, Shape, TextBlock, GraphObject } from "gojs";
 export default {
-  
-}
+  name: "about",
+  data() {
+    return {
+      mockDiagramData: null
+    };
+  },
+  methods: {
+    // init canvas
+    initCanvas() {
+      let _$ = GraphObject.make;
+      let myDiagram = _$(Diagram, "myDiagram");
+      let node = new Node(Panel.Auto);
+      let shape = new Shape();
+      shape.figure = "RoundedRectangle";
+      shape.fill = "lightblue";
+      node.add(shape);
+      let textBlock = new TextBlock();
+      textBlock.text = 'Hello world!';
+      textBlock.margin = 5;
+      node.add(textBlock);
+      myDiagram.add(node);
+    }
+  },
+  mounted() {
+    this.initCanvas();
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
+.parent {
+  margin: 0;
+  padding: 0;
+  .myCanvas {
+    width: 500px;
+    height: 500px;
+    background-color: darkgrey;
+    margin: 0 auto;
+  }
+}
 </style>
