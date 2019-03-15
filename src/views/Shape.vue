85f2dcb072b2017f9dc8e35dd654ab1c2494d217
张家宗 <747026542@qq.com>
2019-03-15 13:47:18 +0800

    嘿嘿

---
+++ src/views/Shape.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script>
+export default {
+  name: "shape",
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {}
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
+
